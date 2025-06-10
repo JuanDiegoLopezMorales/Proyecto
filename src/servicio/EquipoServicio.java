9076fd063c1a99b8b1b8686b0a438e004262204c
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-06-09 20:37:49 -0500

    Update EquipoServicio.java

--- src/servicio/EquipoServicio.java
+++ src/servicio/EquipoServicio.java
@@ -1,4 +1,44 @@
-package Servicio;
+package servicio;
+
+import modelo.Equipo;
+import repositorio.EquipoRepositorio;
+
+import java.util.List;
 
 public class EquipoServicio {
+    private final EquipoRepositorio repositorio;
+    private List<Equipo> equipos;
+
+    public EquipoServicio() {
+        this.repositorio = new EquipoRepositorio();
+        this.equipos = repositorio.cargar();
+    }
+
+    public void registrarEquipo(Equipo equipo) {
+        equipos.add(equipo);
+        repositorio.guardar(equipos);
+    }
+
+    public List<Equipo> listarEquipos() {
+        return equipos;
+    }
+
+    public boolean eliminarEquipoPorId(int id) {
+        boolean eliminado = equipos.removeIf(e -> e.getId() == id);
+        if (eliminado) {
+            repositorio.guardar(equipos);
+        }
+        return eliminado;
+    }
+
+    public boolean actualizarEquipo(int id, Equipo nuevo) {
+        for (int i = 0; i < equipos.size(); i++) {
+            if (equipos.get(i).getId() == id) {
+                equipos.set(i, nuevo);
+                repositorio.guardar(equipos);
+                return true;
+            }
+        }
+        return false;
+    }
 }
